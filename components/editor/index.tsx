5721a10e61f31bed31d174da148d9ed84cc4d1f8
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-08-22 13:54:16 +1000

    Implement Multiple Tags Input

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -55,7 +55,7 @@ const Editor = ({
       ref={editorRef}
       markdown= {value}
       onChange={fieldChange}
-      className='background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border'
+      className='background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border gtrid'
       plugins={[
         headingsPlugin(),
         listsPlugin(),

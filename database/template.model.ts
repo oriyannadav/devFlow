f757e4315e27249c8bf662f0f612df5baaed90fc
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-08-27 08:46:35 +1000

    create a template model

---
+++ database/template.model.ts
@@ -0,0 +1,16 @@
+import { model, models, Schema, Types } from 'mongoose';
+
+export interface IModel {
+}
+
+const ModelSchema = new Schema<IModel>(
+    {
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Model = models?.model || model<IModel>('Model', ModelSchema);
+
+export default Model;

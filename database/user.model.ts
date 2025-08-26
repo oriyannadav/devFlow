4a40b5e4b7a11d354a2d7479067a52054f2951a4
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-08-27 08:13:14 +1000

    Create User Model

---
+++ database/user.model.ts
@@ -0,0 +1,33 @@
+import { model, models, Schema } from 'mongoose';
+
+export interface IUser {
+    name: string;
+    username: string;
+    email: string;
+    bio?: string;
+    image: string;
+    location?: string;
+    portfolio?: string;
+    reputation?: number;
+    password: string;
+}
+
+const UserSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        bio: { type: String },
+        image: { type: String, required: true },
+        location: { type: String },
+        portfolio: { type: String },
+        reputation: { type: Number, default: 0 },
+        password: { type: String, required: true },
+    }, 
+    { 
+        timestamps: true 
+    });
+
+const User = models?.user || model<IUser>('User', UserSchema);
+
+export default User;

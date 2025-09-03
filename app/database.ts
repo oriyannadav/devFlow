2886f12ecca0927dae943d0ae83915a01705ee20
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-09-03 15:51:56 +1000

    CRUD Tickets

---
+++ app/database.ts
@@ -0,0 +1,64 @@
+const tickets = [
+    {
+        id: 1,
+        name: "Ticket 1",
+        status: "open",
+        type: 'bug',
+    },
+    {
+        id: 2,
+        name: "Ticket 2",
+        status: "open",
+        type: 'feature',
+    },
+    {
+        id: 3,
+        name: "Ticket 3",
+        status: "open",
+        type: 'bug',
+    },
+    {
+        id: 4,
+        name: "Ticket 4",
+        status: "open",
+        type: 'feature',
+    },
+    {
+        id: 5,
+        name: "Ticket 5",
+        status: "open",
+        type: 'bug',
+    },
+    {
+        id: 6,
+        name: "Ticket 6",
+        status: "open",
+        type: 'feature',
+    },
+    {
+        id: 7,
+        name: "Ticket 7",
+        status: "open",
+        type: 'bug',
+    },
+    {
+        id: 8,
+        name: "Ticket 8",
+        status: "open",
+        type: 'feature',
+    },
+    {
+        id: 9,
+        name: "Ticket 9",
+        status: "open",
+        type: 'bug',
+    },
+    {
+        id: 10,
+        name: "Ticket 10",
+        status: "open",
+        type: 'feature',
+    }
+];
+
+export default tickets;

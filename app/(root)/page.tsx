ee43b80b7ff8320906406127c046ee6f578d0bec
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-09-03 15:49:51 +1000

    Implement Logging

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button';
 import ROUTES from '@/constants/routes';
 import Link from 'next/link';
 import React from 'react'
+import handleError from '@/lib/handlers/error';
+import { ValidationError } from '@/lib/http-errors';
 
 const questions = [
   {
@@ -47,11 +49,25 @@ const questions = [
   },
 ];
 
+const test = async () => {
+  try {
+    throw new ValidationError({
+      title: ['Required'],
+      tags: ['"JavaScript" is not a valid tag.']
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string}>
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const result = await test();
+  console.log(result);
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { RequestError, ValidationError } from "../http-errors";
 import { ZodError } from "zod";
+import logger from "../logger";
 
 export type ResponseType = 'api' | 'server';
 
@@ -28,6 +29,10 @@ const handleError = (
     responseType: ResponseType = 'server'
 ) => {
     if (error instanceof RequestError) {
+        logger.error(
+            { err: error },
+            `${responseType.toUpperCase()} Error: ${error.message}`
+        );
         return formatResponse(
             responseType,
             error.statusCode,
@@ -41,6 +46,11 @@ const handleError = (
             error.flatten().fieldErrors as Record<string, string[]>
         );
 
+        logger.error(
+            { err: error },
+            `Validation Error: ${validationError.message}`
+        );
+
         return formatResponse(
             responseType,
             validationError.statusCode,
@@ -50,6 +60,8 @@ const handleError = (
     }
 
     if (error instanceof Error) {
+        logger.error(error.message);
+
         return formatResponse(
             responseType,
             500,
@@ -57,6 +69,10 @@ const handleError = (
         );
     }
 
+    logger.error(
+        { err: error },
+        'An unexpected error occurred'
+    );
     return formatResponse(
         responseType,
         500,

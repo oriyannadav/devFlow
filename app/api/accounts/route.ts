911f867ca7824b14db3d1008ac94b8169caef551
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-09-04 13:24:36 +1000

    Implement Create & Read Account Routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,46 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await dbConnect();
+
+        const accounts = await Account.find();
+
+        return NextResponse.json({ success: true, data: accounts}, { status: 200 });
+
+    } catch (error) {
+        return handleError(error, 'api') as APIErrorResponse;
+
+    }
+}
+
+// Create User
+export async function POST(request: Request) {
+    try {
+        await dbConnect();
+
+        const body = await request.json();
+
+        const validateData = AccountSchema.parse(body);
+
+        const existingAccount = await Account.findOne({ 
+            provider: validateData.provider,
+            providerAccountId: validateData.providerAccountId
+         });
+        if (existingAccount) throw new ForbiddenError('Account with the same provider already exists');
+
+        const newAccount = await Account.create(validateData);
+
+        return NextResponse.json({ success: true, data: newAccount }, { status: 201 });
+
+    } catch (error) {
+        return handleError(error, 'api') as APIErrorResponse;
+
+    }
+}

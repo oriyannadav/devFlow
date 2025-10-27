1f3a720bb89e767ffe3985acf356be0291cfc93b
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-10-27 13:01:55 +1000

    Increment Question Views (Approach #3)

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -8,15 +8,16 @@ import { formatNumber, getTimeStamp } from '@/lib/utils';
 import { RouteParams, Tag } from '@/types/global'
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { after } from 'next/server';
 import React from 'react'
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
+  const { success, data: question } = await getQuestion({ questionId: id });
 
-  const [ _, { success, data: question } ] = await Promise.all([
-    await incrementViews({ questionId: id }),
-    await getQuestion({ questionId: id })
-  ])
+  after(async () => {
+    await incrementViews({ questionId: id });
+  })
 
   if (!success || !question) return redirect('/404');
 

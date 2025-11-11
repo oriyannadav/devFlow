6e2d5125d5a196e0209ba990c7442cbeabca506b
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-11-11 11:33:25 +1000

    Implement Edit Delete Buttons UI

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -5,67 +5,76 @@ import React from 'react'
 import TagCard from './TagCard';
 import Metric from '../Metric';
 import { Question, Tag } from '@/types/global';
+import EditDeleteAction from '../user/EditDeleteAction';
 
 interface Props {
     question: Question;
+    showActionBtns?: boolean;
 }
 
-const QuestionCard = ({ question: { _id, title, tags, author, createdAt, upvotes, answers, views } }: Props) => {
+const QuestionCard = ({ 
+    question: { _id, title, tags, author, createdAt, upvotes, answers, views }, 
+    showActionBtns = false,
+}: Props) => {
   return (
     <div className='card-wrapper rounded-[10px] p-9 sm:px-11'>
-        <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
-            <div>
+        <div className="flex flex-col-reverse items-center justify-between gap-5 sm:flex-row">
+            <div className='flex-1'>
                 <span className='subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden'>{getTimeStamp(createdAt)}</span>
                 <Link href={ROUTES.QUESTION(_id)}>
                     <h3 className='sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1'>{title}</h3>
                 </Link>
+            </div>
 
-                <div className='mt-3.5 flex w-full flex-wrap gap-2'>
-                    {tags.map((tag: Tag) => (
-                        <TagCard 
-                            key={tag._id} 
-                            _id={tag._id}
-                            name={tag.name}
-                            compact
-                        />
-                    ))}
-                </div>
+            {showActionBtns && (
+                <EditDeleteAction type='Question' itemId={_id} />
+            )}
+        </div>
 
-                <div className="flex-between mt-6 w-full flex-wrap gap-3">
-                    <Metric 
-                        imgUrl={author.image}
-                        alt={author.name}
-                        value={author.name}
-                        title={`• asked ${getTimeStamp(createdAt)}`}
-                        href={ROUTES.PROFILE(author._id)}
-                        textStyles='body-medium text-dark400_light700'
-                        isAuthor
-                    />
+        <div className='mt-3.5 flex w-full flex-wrap gap-2'>
+            {tags.map((tag: Tag) => (
+                <TagCard 
+                    key={tag._id} 
+                    _id={tag._id}
+                    name={tag.name}
+                    compact
+                />
+            ))}
+        </div>
 
-                    <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
-                        <Metric 
-                            imgUrl= '/icons/like.svg'
-                            alt= 'Like'
-                            value= {upvotes}
-                            title= ' Votes'
-                            textStyles= 'small-medium text-dark400_light800'
-                        />
-                        <Metric 
-                            imgUrl= '/icons/message.svg'
-                            alt= 'Answer'
-                            value= {answers}
-                            title= ' Answers'
-                            textStyles= 'small-medium text-dark400_light800'
-                        />
-                        <Metric 
-                            imgUrl= '/icons/eye.svg'
-                            alt= 'Views'
-                            value= {views}
-                            title= ' Views'
-                            textStyles= 'small-medium text-dark400_light800'
-                        />
-                    </div>
-                </div>
+        <div className="flex-between mt-6 w-full flex-wrap gap-3">
+            <Metric 
+                imgUrl={author.image}
+                alt={author.name}
+                value={author.name}
+                title={`• asked ${getTimeStamp(createdAt)}`}
+                href={ROUTES.PROFILE(author._id)}
+                textStyles='body-medium text-dark400_light700'
+                isAuthor
+            />
+
+            <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
+                <Metric 
+                    imgUrl= '/icons/like.svg'
+                    alt= 'Like'
+                    value= {upvotes}
+                    title= ' Votes'
+                    textStyles= 'small-medium text-dark400_light800'
+                />
+                <Metric 
+                    imgUrl= '/icons/message.svg'
+                    alt= 'Answer'
+                    value= {answers}
+                    title= ' Answers'
+                    textStyles= 'small-medium text-dark400_light800'
+                />
+                <Metric 
+                    imgUrl= '/icons/eye.svg'
+                    alt= 'Views'
+                    value= {views}
+                    title= ' Views'
+                    textStyles= 'small-medium text-dark400_light800'
+                />
             </div>
         </div>
     </div>

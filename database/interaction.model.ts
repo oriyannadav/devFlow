c6d75d32d5b8aec3c301acddb9a5f284e76f0944
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-11-11 15:24:59 +1000

    Build interactions and reputation

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -8,10 +8,13 @@ export interface IInteraction {
 }
 
 export interface IInteractionDoc extends IInteraction, Document {}
+ 
+export const InteractionActionEnums = [ "view", "upvote", "downvote", "bookmark", "post", "edit", "delete", "search", ] as const;
+
 const InteractionSchema = new Schema<IInteraction>(
     {
         user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-        action: { type: String, required: true }, // 'upvote', 'downvote', 'view', 'ask_question',
+        action: { type: String, enum: InteractionActionEnums, required: true },
         actionId: { type: Schema.Types.ObjectId, required: true }, // 'questionId', 'answerId',
         actionType: { type: String, enum: ["question", "answer"], required: true },
     },
--- lib/validations.ts
+++ lib/validations.ts
@@ -1,4 +1,5 @@
- import { z } from 'zod';
+ import { InteractionActionEnums } from '@/database/interaction.model';
+import { z } from 'zod';
 
 export const SignInSchema = z.object({
     email: z
@@ -222,4 +223,11 @@ export const DeleteQuestionSchema = z.object({
 
 export const DeleteAnswerSchema = z.object({
     answerId: z.string().min(1, "Answer ID is required"),
+});
+
+export const CreateInteractionSchema = z.object({
+    action: z.enum(InteractionActionEnums),
+    actionTarget: z.enum(["question", "answer"]),
+    actionId: z.string().min(1),
+    authorId: z.string().min(1),
 });

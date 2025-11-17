80197687cf37e0cb2bfc098c7dc340558cb28f5d
Oriyan Nadav <git@github.com:OriyanN/propellproperty.git>
2025-11-17 11:11:55 +1000

    Adjusted width on loading pages

--- app/(root)/community/loading.tsx
+++ app/(root)/community/loading.tsx
@@ -7,7 +7,7 @@ const loading = () => {
 
             <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
                 <Skeleton className="h-14 flex-1" />
-                <Skeleton className="h-14 w-28" />
+                <Skeleton className="h-14 w-38" />
             </div>
 
             <div className="mt-12 flex flex-wrap gap-5">

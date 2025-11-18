4be542c983dc1802bae674eac2e5fce6d666a6d8
Oriyan Nadav <oriyannadav5@gmail.com>
2025-11-18 14:16:30 +1000

    changed the public api url

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,8 @@ import { IUser } from "@/database/user.model";
 import { fetchHandler } from "./handlers/fetch";
 import { SignInWithOAuthSchema } from "@/types/action";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://devflowww.vercel.app/api";
 
 export const api = {
     auth: {
